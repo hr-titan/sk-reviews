9c1cfaeb04d798c69a6c33396a533971fe19d0ca
stevkim <stevenn.kkim@gmail.com>
2023-12-08 13:02:51 -0800

    Refactored some queries to make code cleaner. Utilized promise.all for multi-query endpoints

--- db.js
+++ db.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+const updateMetaData = require('./data/createMetaData.js');
 
 mongoose.connect('mongodb://127.0.0.1:27017/atelier-rating-reviews')
-  .then(() => console.log('Connection to DB successful: ', mongoose.connection.name))
-  .catch(err => console.log('Error: DB connection unsuccessful', err));
+  .then(() => {
+    console.log('Connection to DB successful: ', mongoose.connection.name)
+    // updateMetaData(mongoose.connection);
+  })
+  .catch(err => console.log('Error: DB connection unsuccessful: ', err));
 
   module.exports = mongoose.connection;
---
+++ script.js
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '10s', target: 3000 },
+    { duration: '15s', target: 4000 },
+    { duration: '5s', target: 0}
+  ]
+};
+
+export default function() {
+  const random = Math.floor(Math.random() * 40000);
+  http.get(`http://localhost:3010/api/reviews/?product_id=${random}`);
+  sleep(1);
+};
